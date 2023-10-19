22afbf1c54ee0b430380a0b79ffd237d0d3e1ca8
Jose Mendoza <josemendozajrr@gmail.com>
2023-10-19 12:03:55 -0700

    completed

---
+++ src/components/Blue.jsx
@@ -0,0 +1,8 @@
+const Blue = () => {
+    return (
+    <div className= "Blue">
+        <h1> Blue </h1>
+    </div>
+    )
+}
+export default Blue
---
+++ src/components/Red.jsx
@@ -0,0 +1,8 @@
+const Red = () => {
+    return (
+    <div className= "Red">
+        <h1> Red </h1>
+    </div>
+    )
+}
+export default Red 
